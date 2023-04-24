2f795c3fcd119f1e2f13a7a1360a4a083d52e895
dante1879 <sirenko.alex777@gmail.com>
2023-04-25 00:21:46 +0300

    task 2 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,11 +1,11 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-import { galleryItems } from "./gallery-items";
+import { galleryItems } from './gallery-items';
 // Change code below this line
 
 // Create gallery markup
-const galleryList = document.querySelector(".gallery");
+const galleryList = document.querySelector('.gallery');
 
 function createGalleryMarkup(items) {
   return items
@@ -23,20 +23,19 @@ function createGalleryMarkup(items) {
     </li>
         `
     )
-    .join("");
+    .join('');
 }
 
 const galleryMarkup = createGalleryMarkup(galleryItems);
-galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
 
-
-var simpleLightbox = new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
-  captionDelay: 250
+var simpleLightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
 });
 
-simpleLightbox.on("show.simplelightbox", function () {
-  console.log("Image opened");
+simpleLightbox.on('show.simplelightbox', function () {
+  console.log('Image opened');
 });
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+function updateLocalStorage(data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+}
+
+player.on('timeupdate', throttle(updateLocalStorage, 1000));
+
+try {
+  player.setCurrentTime(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+} catch (error) {
+  console.log(error);
+}
